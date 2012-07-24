8552f74957bff0a706303b2e78b376cb66ae383a
Christoph DeMaskey <christoph.demaskey@gmail.com>
2012-07-24 15:53:42 -0400

    Add nuget spec and Web.config.transform for nuget package support. Update the AssemblyInfo.cs Removed packages not required to support UpshotHelper Remove assembly references that are not used.

--- UpshotHelper/Properties/AssemblyInfo.cs
+++ UpshotHelper/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UpshotHelper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Html Helper extension to initialize Upshot for use with a WebApi Controller.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Christoph DeMaskey")]
 [assembly: AssemblyProduct("UpshotHelper")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
