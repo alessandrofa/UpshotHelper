4b65ef801819d50e810cf4d94be88e2ebd4b0c31
Christoph DeMaskey <christoph.demaskey@gmail.com>
2012-08-08 15:51:15 -0400

    Update the assembly version, create a new nuget package, updated the release notes.

--- UpshotHelper/Properties/AssemblyInfo.cs
+++ UpshotHelper/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UpshotHelper")]
-[assembly: AssemblyDescription("Html Helper extension to initialize Upshot for use with a WebApi Controller.")]
+[assembly: AssemblyDescription("Html Helper extension to initialize Upshot. Base WebApi Controller to process Upshot submissions.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Christoph DeMaskey")]
 [assembly: AssemblyProduct("UpshotHelper")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
